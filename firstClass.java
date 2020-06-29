6388a8cd35526197f5da05d87acbac511c5a0064
swathireddyy <66693540+swathireddyy@users.noreply.github.com>
2020-06-29 12:27:26 +0530

    Update firstClass.java
    changes at remote branch

--- firstClass.java
+++ firstClass.java
@@ -1,7 +1,7 @@
 public class firstClass{
 	public static void main(String args[])
 {
-		System.out.println("My first java program is updated again in new branch");
+		System.out.println("My first java program is updated again in new branch,done");
 }
 }
 
