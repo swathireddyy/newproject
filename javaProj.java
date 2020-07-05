d90539e735bb120edeb405cf21be724a655c1366
swathireddyy <swathithillana12@gmail.com>
2020-07-05 11:46:37 +0530

    java calculation program written

---
+++ javaProj.java
@@ -0,0 +1,15 @@
+class javaProj{
+	public static void main(String[] args){
+	System.out.println("java calculation program");
+}
+	public int add(){
+		int a=10;
+		int b=20;
+		return (a+b);
+}
+	public int mul(){
+		int a=4;
+		int b=5;
+		return (a*b);
+}
+}
